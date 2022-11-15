400e9183f9738e937c4fdb4f42864846e06b77c9
NicolasGiry <nicolas.giryfr@gmail.com>
2022-11-15 08:40:30 +0100

    clan yakuzas

--- PierreSabre/src/personnages/Yakuza.java
+++ PierreSabre/src/personnages/Yakuza.java
@@ -9,6 +9,12 @@ public class Yakuza extends Humain{
 		this.clan = clan;
 	}
 	
+	@Override
+	public void direBonjour() {
+		super.direBonjour();
+		parler("Mon clan est celui de " + clan);
+	}
+	
 	public void extorquer(Commercant victime) {
 		int argentVictime = victime.getArgent();
 		
